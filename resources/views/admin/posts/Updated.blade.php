bfa5deafb167b9dc37c506533a7056442606ef8e
VTPhu <94094819+VTPhu@users.noreply.github.com>
2022-11-09 12:46:36 +0700

    bo

--- resources/views/admin/posts/Updated.blade.php
+++ resources/views/admin/posts/Updated.blade.php
@@ -33,140 +33,15 @@
                                     <i class="mdi mdi-menu"></i>
                                 </button>
                             </li>
-                            <li class="hidden-xs">
-                                <form role="search" class="app-search">
-                                    <input type="text" placeholder="Search..."
-                                           class="form-control">
-                                    <a href=""><i class="fa fa-search"></i></a>
-                                </form>
-                            </li>
-                            <li class="hidden-xs">
-                                <a href="#" class="menu-item">New</a>
-                            </li>
-                            <li class="dropdown hidden-xs">
-                                <a data-toggle="dropdown" class="dropdown-toggle menu-item" href="#" aria-expanded="false">English
-                                    <span class="caret"></span></a>
-                                <ul role="menu" class="dropdown-menu">
-                                    <li><a href="#">German</a></li>
-                                    <li><a href="#">French</a></li>
-                                    <li><a href="#">Italian</a></li>
-                                    <li><a href="#">Spanish</a></li>
-                                </ul>
-                            </li>
+                           
+                           
                         </ul>
 
                         <!-- Right(Notification) -->
                         <ul class="nav navbar-nav navbar-right">
-                            <li>
-                                <a href="#" class="right-menu-item dropdown-toggle" data-toggle="dropdown">
-                                    <i class="mdi mdi-bell"></i>
-                                    <span class="badge up bg-success">4</span>
-                                </a>
-
-                                <ul class="dropdown-menu dropdown-menu-right arrow-dropdown-menu arrow-menu-right dropdown-lg user-list notify-list">
-                                    <li>
-                                        <h5>Notifications</h5>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="user-list-item">
-                                            <div class="icon bg-info">
-                                                <i class="mdi mdi-account"></i>
-                                            </div>
-                                            <div class="user-desc">
-                                                <span class="name">New Signup</span>
-                                                <span class="time">5 hours ago</span>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="user-list-item">
-                                            <div class="icon bg-danger">
-                                                <i class="mdi mdi-comment"></i>
-                                            </div>
-                                            <div class="user-desc">
-                                                <span class="name">New Message received</span>
-                                                <span class="time">1 day ago</span>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="user-list-item">
-                                            <div class="icon bg-warning">
-                                                <i class="mdi mdi-settings"></i>
-                                            </div>
-                                            <div class="user-desc">
-                                                <span class="name">Settings</span>
-                                                <span class="time">1 day ago</span>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li class="all-msgs text-center">
-                                        <p class="m-0"><a href="#">See all Notification</a></p>
-                                    </li>
-                                </ul>
-                            </li>
-
-                            <li>
-                                <a href="#" class="right-menu-item dropdown-toggle" data-toggle="dropdown">
-                                    <i class="mdi mdi-email"></i>
-                                    <span class="badge up bg-danger">8</span>
-                                </a>
-
-                                <ul class="dropdown-menu dropdown-menu-right arrow-dropdown-menu arrow-menu-right dropdown-lg user-list notify-list">
-                                    <li>
-                                        <h5>Messages</h5>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="user-list-item">
-                                            <div class="avatar">
-                                                <img src="/assets/images/users/avatar-2.jpg" alt="">
-                                            </div>
-                                            <div class="user-desc">
-                                                <span class="name">Patricia Beach</span>
-                                                <span class="desc">There are new settings available</span>
-                                                <span class="time">2 hours ago</span>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="user-list-item">
-                                            <div class="avatar">
-                                                <img src="admin/assets/images/users/avatar-3.jpg" alt="">
-                                            </div>
-                                            <div class="user-desc">
-                                                <span class="name">Connie Lucas</span>
-                                                <span class="desc">There are new settings available</span>
-                                                <span class="time">2 hours ago</span>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="user-list-item">
-                                            <div class="avatar">
-                                                <img src="admin/assets/images/users/avatar-4.jpg" alt="">
-                                            </div>
-                                            <div class="user-desc">
-                                                <span class="name">Margaret Becker</span>
-                                                <span class="desc">There are new settings available</span>
-                                                <span class="time">2 hours ago</span>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li class="all-msgs text-center">
-                                        <p class="m-0"><a href="#">See all Messages</a></p>
-                                    </li>
-                                </ul>
-                            </li>
-
-                            <li>
-                                <a href="javascript:void(0);" class="right-bar-toggle right-menu-item">
-                                    <i class="mdi mdi-settings"></i>
-                                </a>
-                            </li>
-
                             <li class="dropdown user-box">
                                 <a href="" class="dropdown-toggle waves-effect user-link" data-toggle="dropdown" aria-expanded="true">
-                                    <img src="admin/assets/images/users/avatar-1.jpg" alt="user-img" class="img-circle user-img">
+                                    <img src="/admin/assets/images/users/avatar-1.jpg" alt="user-img" class="img-circle user-img">
                                 </a>
 
                                 <ul class="dropdown-menu dropdown-menu-right arrow-dropdown-menu arrow-menu-right user-list notify-list">
